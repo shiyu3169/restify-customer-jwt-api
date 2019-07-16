152e55da1ea3f8e7f58ea0fb10655fa62cda29b1
Shiyu Wang <shiyu3169@gmail.com>
2019-07-16 12:06:43 -0400

    protect specific route"

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
 // Protect Routes
-server.use(rjwt({ secret: config.JWT_SECRET }).unless({ path: ['/auth'] }));
+// server.use(rjwt({ secret: config.JWT_SECRET }).unless({ path: ['/auth'] }));
 
 server.listen(config.PORT, () => {
   mongoose.connect(config.MONGODB_URI, {
--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,7 @@
 const errors = require('restify-errors');
-
+const rjwt = require('restify-jwt-community');
 const Customer = require('../models/Customer');
+const config = require('../config');
 
 module.exports = server => {
   // Get Customers
@@ -30,58 +31,74 @@ module.exports = server => {
   });
 
   // Add Customer
-  server.post('/customers', async (req, res, next) => {
-    // Check for JSON
-    if (!req.is('application/json')) {
-      return next(new errors.InvalidContentError("Expects 'application/json'"));
+  server.post(
+    '/customers',
+    rjwt({ secret: config.JWT_SECRET }),
+    async (req, res, next) => {
+      // Check for JSON
+      if (!req.is('application/json')) {
+        return next(
+          new errors.InvalidContentError("Expects 'application/json'")
+        );
+      }
+
+      const { name, email, balance } = req.body;
+
+      const customer = new Customer({
+        name,
+        email,
+        balance
+      });
+
+      try {
+        await customer.save();
+        res.send(201);
+        next();
+      } catch (err) {
+        return next(new errors.InternalError(err.message));
+      }
     }
-
-    const { name, email, balance } = req.body;
-
-    const customer = new Customer({
-      name,
-      email,
-      balance
-    });
-
-    try {
-      await customer.save();
-      res.send(201);
-      next();
-    } catch (err) {
-      return next(new errors.InternalError(err.message));
-    }
-  });
+  );
 
   // Update Customer
-  server.put('/customers/:id', async (req, res, next) => {
-    // Check for JSON
-    if (!req.is('application/json')) {
-      return next(new errors.InvalidContentError("Expects 'application/json'"));
-    }
+  server.put(
+    '/customers/:id',
+    rjwt({ secret: config.JWT_SECRET }),
+    async (req, res, next) => {
+      // Check for JSON
+      if (!req.is('application/json')) {
+        return next(
+          new errors.InvalidContentError("Expects 'application/json'")
+        );
+      }
 
-    try {
-      await Customer.findOneAndUpdate({ _id: req.params.id }, req.body);
-      res.send(200);
-      next();
-    } catch (err) {
-      return next(
-        new errors.ResourceNotFoundError(
-          `There is not customer with the id of ${req.params.id}`
-        )
-      );
+      try {
+        await Customer.findOneAndUpdate({ _id: req.params.id }, req.body);
+        res.send(200);
+        next();
+      } catch (err) {
+        return next(
+          new errors.ResourceNotFoundError(
+            `There is not customer with the id of ${req.params.id}`
+          )
+        );
+      }
     }
-  });
+  );
 
   // Delete Customer
-  server.del('/customers/:id', async (req, res, next) => {
-    try {
-      await Customer.findOneAndRemove({ _id: req.params.id });
-      res.send(204);
-    } catch (err) {
-      new errors.ResourceNotFoundError(
-        `There is not customer with the id of ${req.params.id}`
-      );
+  server.del(
+    '/customers/:id',
+    rjwt({ secret: config.JWT_SECRET }),
+    async (req, res, next) => {
+      try {
+        await Customer.findOneAndRemove({ _id: req.params.id });
+        res.send(204);
+      } catch (err) {
+        new errors.ResourceNotFoundError(
+          `There is not customer with the id of ${req.params.id}`
+        );
+      }
     }
-  });
+  );
 };
